ce0c44b829b53d075d21321f7f56fc4d88b44a9a
e <pjduan@vip.qq.com>
2018-09-05 10:24:05 +0800

    Initial commit

---
+++ Eureka-Client/src/main/java/com/arsenal/eurekaclient/EurekaClientApplication.java
@@ -0,0 +1,39 @@
+package com.arsenal.eurekaclient;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/** 
+* @Title: 服务提供者
+* @author duanpj
+* @createtime 2018/9/5 9:28
+*/
+@RestController
+@EnableEurekaClient
+@SpringBootApplication
+public class EurekaClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaClientApplication.class, args);
+    }
+
+    @Value("${server.port}")
+    String port;
+
+    /**
+     * defaultValue:默认
+     * miAir = name
+     * http://localhost:8762/helloDuanpj?miAir=wwwww
+     * */
+    @RequestMapping("/helloDuanpj")
+    public String home(@RequestParam(value = "miAir", defaultValue = "Aersenal") String name){
+        return "hi " + name + " ,i am from port:" + port;
+    }
+
+
+}
---
+++ Eureka-Server/src/main/java/com/arsenal/eurekaserver/EurekaServerApplication.java
@@ -0,0 +1,19 @@
+package com.arsenal.eurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+* @Title: 服务注册中心
+* @author duanpj
+* @createtime 2018/9/5 9:29
+*/
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
+}
