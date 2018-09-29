0bb0a557ac19f225ac787fdc4838a13e85646c35
pjduan <pjduan@vip.qq.com>
2018-09-29 16:39:19 +0800

    加入logback日志组件

--- Eureka-Server/src/main/java/com/arsenal/eurekaserver/controller/StudentController.java
+++ Eureka-Server/src/main/java/com/arsenal/eurekaserver/controller/StudentController.java
@@ -2,13 +2,13 @@ package com.arsenal.eurekaserver.controller;
 
 import com.arsenal.eurekaserver.inter.StudentRepository;
 import com.arsenal.eurekaserver.model.Student;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-
 /**
  * @Auther: Pjduan
  * @Date: 2018/9/26 16:06
@@ -18,8 +18,11 @@ public class StudentController {
     @Autowired
     private StudentRepository studentRepository;
 
+    private final static Logger logger = LoggerFactory.getLogger(StudentController.class);
+
     @GetMapping("/getAllStudent")
     public List<Student> getAllStudent() {
+        logger.info("访问getAllStudent方法");
         return studentRepository.findAll();
     }
 }
